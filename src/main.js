5960186741538ea4d949057468a885fcd5f35ac6
Cooper <me@loertis.dev>
2020-08-22 20:15:02 -0500

    updated

--- src/main.js
+++ src/main.js
@@ -3,18 +3,27 @@ window.onload = function() {
     /* Usage Detection */
 
     var webUrl = location.hostname;
-    if (webUrl == "localhost" || webUrl == "coopertsoris.dev") {
-        var link = document.createElement("link")
-        link.rel = "stylesheet"
-        link.type = "text/css"
-        link.href = "/main.css"
-        document.getElementsByTagName("head")[0].appendChild(link)
-        console.log("CSS Loaded")
-    } else {
+    if (!webUrl == "localhost" || !webUrl == "coopertsoris.dev") {
         document.body.remove()
-        alert("Please don't steal the source code.\nYour location is being tracked.\n© Cooper Tsoris 2020")
-        while (true) {
-            console.log("You stole the source-code!")
-        }
+        alert("Please don't steal the source code.\n© Cooper Tsoris 2020")
+    }
+
+    confetti({
+        angle: 60,
+        spread: 50,
+        startVelocity: 50,
+        origin: { x: 0, y:1}
+    });
+    confetti({
+        angle: 120,
+        spread: 50,
+        startVelocity: 50,
+        origin: { x: 1, y:1 }
+    });
+
+    var content = document.getElementById("ct")
+    content.addEventListener("animationend", contentFadedInComplete);
+    function contentFadedInComplete() {
+        content.style.opacity = "100%"
     }
 };
