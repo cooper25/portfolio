f6bf3014a6a7991f3162cefe024fc56f60f54d6e
cooper25 <me@loertis.dev>
2020-11-17 11:26:11 -0600

    11/17/20

---
+++ src/assets/scripts/index.js
@@ -0,0 +1,6 @@
+// Copy Notice
+
+if (location.hostname !== "coopertsoris.dev") {
+    document.body.remove()
+    alert("Please don't steal the source code.\nYour location is being tracked.\n© Cooper Tsoris 2020")
+}
